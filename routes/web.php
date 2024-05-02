3d204c1e8ea024e38cf9fe77d6aa9bf281d81f11
Alexandre Bintz <alexandre@bintz.io>
2024-05-02 18:27:36 +0200

    redirect index to blogposts list

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BlogPostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('blogposts.index');
 });
 
 Route::resource('blogposts', BlogPostController::class);
