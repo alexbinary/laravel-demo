1d394b67279c4088c51c22ed3f06093367a6ac6c
Alexandre Bintz <alexandre@bintz.io>
2024-05-08 14:59:41 +0200

    form component

--- resources/js/Pages/BlogPosts/Create.vue
+++ resources/js/Pages/BlogPosts/Create.vue
@@ -5,6 +5,7 @@ import { useForm } from '@inertiajs/vue3'
 import Layout from '../Layout.vue'
 import Title from '../Title.vue'
 import HomeLink from '../HomeLink.vue'
+import BlogPostForm from '../BlogPostForm.vue'
 
 const form = useForm({
   title: null,
@@ -22,36 +23,7 @@ const form = useForm({
 
         <Title>New post</Title>
 
-        <form @submit.prevent="form.post($route('blogposts.store'))">
+        <BlogPostForm v-model="form" @submit="form.post($route('blogposts.store'))"></BlogPostForm>
 
-            <div class="grid">
-            
-                <div class="grid mb-2">
-                    <label for="title">Title</label>
-                    <input type="text" name="title" v-model="form.title" class="border p-2 rounded bg-gray-50">
-                    <p v-if="form.errors.title" class="text-red-800">{{ form.errors.title }}</p>
-                </div>
-
-                <div class="grid mb-2">
-                    <label for="category">Category</label>
-                    <input type="text" name="category" v-model="form.category" class="border p-2 rounded bg-gray-50">
-                    <p v-if="form.errors.category" class="text-red-800">{{ form.errors.category }}</p>
-                </div>
-
-                <div class="grid mb-2">
-                    <label for="content">Content</label>
-                    <textarea name="content" v-model="form.content" class="border p-2 rounded bg-gray-50 h-80"></textarea>
-                    <p v-if="form.errors.content" class="text-red-800">{{ form.errors.content }}</p>
-                </div>
-                
-                <div class="grid mb-2">
-                    <label for="picture">Picture</label>
-                    <input name="picture" type="file" @input="form.picture = $event.target.files[0]" />
-                </div>
-
-            </div>
-
-            <input type="submit" value="Submit" class="border border-gray-400 p-2 rounded-md bg-gray-200 hover:bg-gray-300 mt-4">
-        </form>
     </Layout>
 </template>
--- resources/js/Pages/BlogPosts/Edit.vue
+++ resources/js/Pages/BlogPosts/Edit.vue
@@ -5,6 +5,7 @@ import { useForm } from '@inertiajs/vue3'
 import Layout from '../Layout.vue'
 import Title from '../Title.vue'
 import HomeLink from '../HomeLink.vue'
+import BlogPostForm from '../BlogPostForm.vue'
 
 const { post } = defineProps({ post: Object })
 
@@ -24,37 +25,8 @@ const form = useForm({
         <HomeLink></HomeLink>
 
         <Title>Edit post</Title>
+
+        <BlogPostForm v-model="form" @submit="form.post($route('blogposts.update', { blogpost: post }))"></BlogPostForm>
         
-        <form @submit.prevent="form.post($route('blogposts.update', { blogpost: post }))">
-
-            <div class="grid">
-            
-                <div class="grid mb-2">
-                    <label for="title">Title</label>
-                    <input type="text" name="title" v-model="form.title" class="border p-2 rounded bg-gray-50">
-                    <p v-if="form.errors.title" class="text-red-800">{{ form.errors.title }}</p>
-                </div>
-
-                <div class="grid mb-2">
-                    <label for="category">Category</label>
-                    <input type="text" name="category" v-model="form.category" class="border p-2 rounded bg-gray-50">
-                    <p v-if="form.errors.category" class="text-red-800">{{ form.errors.category }}</p>
-                </div>
-
-                <div class="grid mb-2">
-                    <label for="content">Content</label>
-                    <textarea name="content" v-model="form.content" class="border p-2 rounded bg-gray-50 h-80"></textarea>
-                    <p v-if="form.errors.content" class="text-red-800">{{ form.errors.content }}</p>
-                </div>
-
-                <div class="grid mb-2">
-                    <label for="picture">Picture</label>
-                    <input name="picture" type="file" @input="form.picture = $event.target.files[0]" />
-                </div>
-
-            </div>
-            
-            <input type="submit" value="Submit" class="border border-gray-400 p-2 rounded-md bg-gray-200 hover:bg-gray-300 mt-4">
-        </form>
     </Layout>
 </template>
